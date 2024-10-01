9d92b8cb32a8075557dc730384ec917dbbf91ce7
Adil Khosa <muhammadadilmemon79@gmail.com>
2024-10-01 18:43:14 +0500

    js class practice06 done

---
+++ app.js
@@ -0,0 +1,56 @@
+// Chap 26 Rounding numbers:
+
+
+// var number = +prompt("Enter a decimal number:");
+
+// console.log(Math.round(number));
+
+// console.log(Math.ceil(number));
+
+// console.log(Math.floor(number));
+
+
+// Chap 27 Generating random numbers:
+
+
+// var randomNum = Math.random();
+
+// var randomNum = Math.random() * 6;
+
+// console.log(randomNum);
+
+// console.log(Math.round(randomNum));
+
+
+// Chap 28 Converting strings to integers and decimals:
+
+
+// var age = prompt("Enter your age:");
+
+// console.log(age);
+// console.log(Number(age));
+
+
+// Chap 29 Converting strings to numbers, numbers to strings:
+
+
+// var numAsNum = 1234;
+
+// var numAsStr = numAsNum.toString();
+
+// console.log(numAsNum);
+
+// console.log(numAsStr);
+
+
+// Chap 30 Controlling the length of decimals:
+
+
+// var randomNum = Math.random() * 10;
+
+// console.log(randomNum.toFixed());
+
+
+
+
+// ----------------------------------------------------------------------------------------------
